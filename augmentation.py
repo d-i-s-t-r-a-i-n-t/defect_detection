176b003a5121848d369ab6833f3e1286cef3ee7b
d-i-s-t-r-a-i-n-t <p13275754@gmail.com>
2024-11-05 02:53:01 +0300

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -1,91 +1,113 @@
-import os 
+import cv2
 import numpy as np
 import random
-from torchvision import transforms
-from PIL import Image
+import os
+from skimage.io import imsave
 
-# путь к папке с изображениями и масками
-images_dir = './details_resize/' 
-masks_dir = './decoded_masks/' # (попробовать на черном/мб даже не разъединять по классам/мб из категориальных)
-output_images_dir = './aug_img/'
-output_masks_dir = './aug_masks/'
+input_images_path = './resized_details/'
+input_masks_path = '.\\annotation\\masks_categorical'
 
-# создаем выходные директории, если они не существуют
-os.makedirs(output_images_dir, exist_ok=True)
-os.makedirs(output_masks_dir, exist_ok=True)
+augmented_images_path = '.\\augmentation\\aug_details'
+categorical_masks_path = '.\\augmentation\\aug_masks_categorical'
+colour_masks_path = '.\\augmentation\\aug_masks_colour'
 
-# получаем список всех изображений
-image_files = [f for f in os.listdir(images_dir) if f.endswith('.jpg')]
+os.makedirs(augmented_images_path, exist_ok=True)
+os.makedirs(categorical_masks_path, exist_ok=True)
+os.makedirs(colour_masks_path, exist_ok=True)
 
-# определяем класс для аугментации
-class ImageMaskTransform:
-    def __init__(self):
-        self.flip_horizontal = transforms.RandomHorizontalFlip(p=1)
-        self.flip_vertical = transforms.RandomVerticalFlip(p=1)
-        self.rotation = transforms.RandomRotation(degrees=25)
-        self.color_jitter = transforms.ColorJitter(brightness=0.2, contrast=0.2)
-        self.crop = transforms.RandomCrop(size=(800, 800))
+# случайный поворот
+def random_flip(image, mask):
+    if random.choice([True, False]):
+        image = cv2.flip(image, 1)
+        mask = cv2.flip(mask, 1)
+    if random.choice([True, False]):
+        image = cv2.flip(image, 0)
+        mask = cv2.flip(mask, 0)
+    return image, mask
 
-    def __call__(self, image, mask):
-        # применяем горизонтальное отражение
-        if random.random() < 0.5:
-            image = self.flip_horizontal(image)
-            mask = self.flip_horizontal(mask)
+# случайный кроп
+def random_crop(image, mask, crop_size=(800, 800)):  
+    h, w = image.shape[:2]
+    ch, cw = crop_size
+    x = random.randint(0, max(1, w - cw))
+    y = random.randint(0, max(1, h - ch))
+    image = image[y:y + ch, x:x + cw]
+    mask = mask[y:y + ch, x:x + cw]
+    return image, mask
 
-        # применяем вертикальное отражение
-        if random.random() < 0.5:
-            image = self.flip_vertical(image)
-            mask = self.flip_vertical(mask)
 
-        # поворот
-        rotation_angle = random.uniform(-25, 25)
-        image = transforms.functional.rotate(image, rotation_angle)
-        mask = transforms.functional.rotate(mask, rotation_angle)
+# случайное изменение яркости
+def random_brightness(image):
+    factor = 0.8 + random.uniform(0, 0.7)  
+    return np.clip(image * factor, 0, 255).astype(np.uint8)
 
-        # обрезание (важно, чтобы обрезка применялась к одинаковым координатам)
-        image, mask = self.crop_to_same_area(image, mask)
+# случайный поворота на угол
+def random_rotate(image, mask):
+    angle = random.choice([90, 180, 270])
+    image = cv2.rotate(image, {90: cv2.ROTATE_90_CLOCKWISE, 180: cv2.ROTATE_180, 270: cv2.ROTATE_90_COUNTERCLOCKWISE}[angle])
+    mask = cv2.rotate(mask, {90: cv2.ROTATE_90_CLOCKWISE, 180: cv2.ROTATE_180, 270: cv2.ROTATE_90_COUNTERCLOCKWISE}[angle])
+    return image, mask
 
-        return image, mask
+# преобразование категориальных значений маски в цветное изображение
+def logits2rgb(img):
+    colours = [
+        [0, 0, 0],        # черный (фон)
+        [187, 207, 74],   # зеленый
+        [0, 108, 132],    # синий
+        [255, 204, 184],  # желтый
+        [200, 0, 10],     # красный
+        [226, 232, 228],  # белый
+        [232, 167, 53]    # оранжевый
+    ]
 
-    def crop_to_same_area(self, image, mask):
-        # получаем размер обрезаемого изображения
-        i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(800, 800))
-        image = transforms.functional.crop(image, i, j, h, w)
-        mask = transforms.functional.crop(mask, i, j, h, w)
-        return image, mask
+    h, w = img.shape
+    col = np.zeros((h, w, 3), dtype=np.uint8)
+    for val in np.unique(img):
+        mask = (img == val)
+        col[mask] = colours[int(val)]
+    return col
 
-# функция для аугментации изображения и маски
-def augment_image_and_mask(image_file):
-    # загружаем изображение
-    image_path = os.path.join(images_dir, image_file)
-    image = Image.open(image_path).convert("RGB")  # загружаем изображение
+# аугментация и сохранения
+def augment_and_save(input_images_path, input_masks_path):
 
-    # ищем соответствующие маски
-    base_name = image_file.split('.')[0]  # получаем базовое имя без расширения
-    mask_files = [f for f in os.listdir(masks_dir) if f.startswith(f'decoded_{base_name}') and f.endswith('.png')]
-    
-    transform = ImageMaskTransform()
+    input_images = sorted([os.path.join(input_images_path, f) for f in os.listdir(input_images_path) if f.endswith('.png')])
+    input_masks = sorted([os.path.join(input_masks_path, f) for f in os.listdir(input_masks_path) if f.endswith('.png')])
 
-    # аугментация для каждой маски
-    for i in range(30):  # создаем 30 аугментированных вариантов
-        for mask_file in mask_files:
-            if mask_file.split("_")[1].split(".")[0] == base_name:
-                mask_path = os.path.join(masks_dir, mask_file)
-                mask = Image.open(mask_path).convert("L")  # загружаем маску в градациях серого
+    for i, (img_path, mask_path) in enumerate(zip(input_images, input_masks)):
 
-                # применяем аугментацию
-                augmented_image, augmented_mask = transform(image, mask)
+        image = cv2.imread(img_path)
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
-                # сохраняем аугментированное изображение
-                augmented_image_filename = os.path.join(output_images_dir, f'{base_name}_aug_{i}-mask.png_{mask_file.split("_")[-1]}')
-                augmented_image.save(augmented_image_filename)
+        # сохраняем оригиналы изображения и маски
+        base_name = os.path.splitext(os.path.basename(img_path))[0]
+        imsave(os.path.join(augmented_images_path, f"{base_name}.png"), cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        imsave(os.path.join(categorical_masks_path, f"{base_name}-mask-cat.png"), mask)
+        mask_colour = logits2rgb(mask)
+        imsave(os.path.join(colour_masks_path, f"{base_name}-mask-col.png"), mask_colour)
 
-                # сохраняем аугментированную маску
-                augmented_mask_filename = os.path.join(output_masks_dir, f'{base_name}_aug_{i}-mask.png_{mask_file.split("_")[-1]}')
-                augmented_mask.save(augmented_mask_filename)
+        # применяем аугментации
+        for aug_num in range(1, 4):
+            if aug_num == 1:
+                # случайный флип и кроп
+                image_aug, mask_aug = random_flip(image, mask)
+                image_aug, mask_aug = random_crop(image_aug, mask_aug)
+            elif aug_num == 2:
+                # случайное изменение яркости
+                image_aug = random_brightness(image)
+                mask_aug = mask
+            elif aug_num == 3:
+                # случайный поворот на 90, 180 или 270 градусов
+                image_aug, mask_aug = random_rotate(image, mask)
 
-# последовательная обработка изображений
-for image_file in image_files:
-    augment_image_and_mask(image_file)
+            # сохраняем аугментированные изображения
+            aug_img_name = f"{base_name}-aug_{aug_num}.png"
+            imsave(os.path.join(augmented_images_path, aug_img_name), cv2.cvtColor(image_aug, cv2.COLOR_BGR2RGB))
 
-print("Аугментация завершена!")
+            categorical_mask_name = f"{base_name}-aug_{aug_num}-mask-cat.png"
+            imsave(os.path.join(categorical_masks_path, categorical_mask_name), mask_aug)
+
+            colour_mask_name = f"{base_name}-aug_{aug_num}-mask-col.png"
+            mask_colour_aug = logits2rgb(mask_aug)
+            imsave(os.path.join(colour_masks_path, colour_mask_name), mask_colour_aug)
+
+augment_and_save(input_images_path, input_masks_path)
